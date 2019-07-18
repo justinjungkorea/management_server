3a6e41ffc4ea586aacb48b916198b3a039ca65bb
Dong Wook Jung <jungdw0624@gmail.com>
2019-07-19 00:05:50 +0900

    access-control...

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ const upload = multer({
 
 app.all("*", function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });

caec2d3ab33dfad9890a3587a360e460fc6278f9
Dong Wook Jung <jungdw0624@gmail.com>
2019-07-18 23:43:20 +0900

    middleware!

--- server.js
+++ server.js
@@ -26,20 +26,17 @@ const upload = multer({
     dest: './upload'
 })
 
+app.all("*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 
 app.get('/api/customers', (req,res)=>{
     connection.query("select * from customer where isDeleted=0", (err, rows, fields)=>{
         res.send(rows);
     })
-});
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
 });
 
 app.use('/image', express.static('./upload'));
